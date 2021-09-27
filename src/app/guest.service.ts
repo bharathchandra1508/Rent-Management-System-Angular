b076a2c64e60d0bb6f833e7d4860a310f4366afd
Bharath Chandra Vedanabathula <bharath.chandra.vedanabathula@ibm.com>
2021-09-27 12:16:32 +0530

    Adding service for the guesti

---
+++ src/app/guest.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Guest } from './guest';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuestService {
+
+  private baseURL = "http://localhost:8080/api/guest";
+
+  constructor(private httpClient: HttpClient) { }
+
+  createGuest(guest: Guest): Observable<Object>{
+    return this.httpClient.post(`${this.baseURL}`,guest);
+  }
+
+}
